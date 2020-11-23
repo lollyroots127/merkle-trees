f50f681421a113f0c94cf339a7eb37cee78173fa
Geniz1 <60437609+Geniz1@users.noreply.github.com>
2020-11-23 17:32:12 -0600

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,1 +1,15 @@
+// Program that copies a folder of files into another directory and verifies if the files sent are the same as the ones in current directory.
+// First, it creates a clone of the merkel tree using the root node.
+// Then, it will have to ensure that the files copied are exactly the same and there was no issue in the cloning process by comparing root nodes.
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+int main( int argc, char *argv[] )
+{
+  
+  
+}
