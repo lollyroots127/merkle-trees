8a02bdf3e4ff7d65ed33f5fa3cfd2287ab3126d3
Geniz1 <60437609+Geniz1@users.noreply.github.com>
2020-11-25 18:11:00 -0600

    Update MerkleTree.cpp

--- MerkleTree.cpp
+++ MerkleTree.cpp
@@ -3,3 +3,64 @@
 #include <iostream> 
 #include <cmath>
 using namespace std;
+
+
+/*
+ * this function finds the given key in the Binary Tree
+ * returns the node containing the key
+ * returns NULL in case key is not present
+ */
+int find( int key)
+{
+  if (tree == NULL) 
+  {
+    return NULL;
+  }
+  if (tree->key == key)
+  {
+    return tree;
+  }
+  else if (key < tree->key)
+  {
+    return find(tree->left, key);
+  }
+  else
+  {
+    return find(tree->right, key);
+  }
+}
+
+/* this function inserts the newly created node 
+   in the existing Binary Tree */
+void insertNum(Node *item)
+{
+ 
+  if (item->key < tree->key)
+  {
+    if (tree->left == NULL)
+    {
+      tree->left = item;
+      return;
+    }
+    else
+    {
+      insert_element(tree->left, item);
+      return;
+    }
+  }
+ 
+  else if (item->key > tree->key)
+  {
+    if (tree->right == NULL)
+    {
+      tree->right = item;
+      return;
+    }
+    else
+    {
+      insert_element(tree->right, item);
+      return;
+    }
+  }
+}
+ 
