4d105f3f09ba80f8ef95aa98d5186214e4ce630f
Laura Roots <32853099+lollyroots127@users.noreply.github.com>
2020-11-25 22:31:51 -0600

    Update FileChecker.h

--- FileChecker.h
+++ FileChecker.h
@@ -7,7 +7,7 @@ class FileChecker
     public:
 
     //Function that takes the input of pointers to two trees and compares their hashkeys at the root node
-    void compareHashRoot(tree1,tree2);
+    void compareHashRoot(MerkleTree * tree1,MerkleTree * tree2);
 
     //Function that performs a deep copy of the tree into another pre-existing tree
     void clone( Node *t) const
