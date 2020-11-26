2251f9ce93727ed16db22193e76d924fe2d9f637
Geniz1 <60437609+Geniz1@users.noreply.github.com>
2020-11-25 19:01:56 -0600

    Update FileChecker.h

--- FileChecker.h
+++ FileChecker.h
@@ -1,1 +1,14 @@
+#ifndef FILE_CHECKER_H
+#define FILE_CHECKER_H
 
+class FileCheker
+{
+    
+    //Function that takes the input of pointers to two trees and compares their hashkeys at the root node
+    Node * compareHashRoot(tree1,tree2);
+    
+    //Function that performs a deep copy of the tree into another pre-existing tree
+    Node * clone( Node *t) const
+
+
+}
