172dfe1da5fb70195c8fd2802274d622143ac312
Laura Roots <32853099+lollyroots127@users.noreply.github.com>
2020-11-25 19:00:23 -0600

    Update MerkleTree.h

--- MerkleTree.h
+++ MerkleTree.h
@@ -30,8 +30,8 @@ class MerkleTree
 				right = nullptr;
 				parent = nullptr;
 			};
-			
-			
+
+
 			Node( int key = -1, Node* leftptr = nullptr, Node* rightptr = nullptr,Node* parentptr = nullptr)
 				: hashKey{key}, left{leftptr},right{rightptr},parent{parentptr} {};
 		};
@@ -45,19 +45,25 @@ class MerkleTree
 		 *
 		 */
 		int hash(int num);
-	
+
 
 		/* Returns the root node of the tree
 		 *
 		 */
 		Node* getRoot();
-	
+
+		/*
+		 * this function finds the given key in the Binary Tree
+		 * returns the node containing the key
+		 * returns NULL in case key is not present
+		 */
+		int find( int key);
 
 		/* Inserts a value into the tree, creating a node for it and updating all parent nodes
 		 *
 		 */
 		void insertNum(const int num);
-	
+
 
 		/* Returns the hash key of the root node of the tree
 		 * Compare this to the hash key of another tree to see if the two trees are the same
@@ -65,22 +71,22 @@ class MerkleTree
 		 *
 		 */
 		int getRootHash();
-	
+
 
 		/* Deletes a node from the tree, doing rotations if necessary and updating parent nodes
 		 *
 		 */
 		void deleteNum(int num);
-	
+
 
 		/* Updates the hashkey for a parent node if any data in the child nodes has changed
 		 * Call this when a subtree is changed, the root hash needs to change
 		 * This function should call itself on the parent of the current node once it is done
 		 */
 		void updateHash(Node* ptr);
-	
-	
-		
+
+
+
 
 
 
